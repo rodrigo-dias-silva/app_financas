aeb653a18c404c35ef83c441ed0c697cc2c07dfb
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-10-31 20:00:51 -0300

    estruturaçao do App

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import firebase from './src/services/firebaseConnection';
+import Routes from './src/routes';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Routes />
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
