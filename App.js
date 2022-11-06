647f059632a524031ce167a1ab67c968e0430874
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-11-05 23:37:05 -0300

    adiçao AuthProvider

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-
-import firebase from './src/services/firebaseConnection';
-import Routes from './src/routes';
 import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './src/routes';
+
+import AuthProvider from './src/contexts/auth'
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Routes />
-      <StatusBar style="inverted" />
+      <AuthProvider>
+        <Routes />
+        <StatusBar style="inverted" />
+      </AuthProvider>
     </NavigationContainer>
   );
 }
