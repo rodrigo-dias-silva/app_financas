c4e11dccee1465dd32ccb17cb5302d8a0a41a96d
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-11-30 21:29:00 -0300

    config de cadastro de user no auth e info no database, alem de passar as props para o provider

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,15 +1,32 @@
 import React, { createContext, useState } from "react";
+import firebase from './../services/firebaseConnection';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-  const [user, setUser] = useState({
-    name: 'Rodrigo',
-    uid: '121312323131'
-  })
+  const [user, setUser] = useState(null)
+
+  async function signUp(email, password, nome) {
+    await firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
+        await firebase.database().ref('users').child(uid).set({
+          saldo: 0,
+          name: nome
+        })
+          .then(() => {
+            let data = {
+              uid: uid,
+              name: nome,
+              email: value.user.email,
+            }
+            setUser(data);
+          })
+      })
+  }
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ signed: !!user, user, signUp }}>
       {children}
     </AuthContext.Provider>
   );
