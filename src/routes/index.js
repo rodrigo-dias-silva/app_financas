a92ebf59d37f3b34c37fa1d060ee41a8b153c5a6
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-11-30 23:14:18 -0300

    verificaçao de loading e adiçao icone durante login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,20 @@
 import React, { useContext } from "react";
-import { AuthContext } from "../contexts/auth";
-
+import { ActivityIndicator, View } from "react-native";
 
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
+import { AuthContext } from "../contexts/auth";
 
 function Routes() {
-  const { signed } = useContext(AuthContext)
+  const { signed, loading } = useContext(AuthContext)
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#131313" }}>
+        <ActivityIndicator size={"large"} />
+      </View>
+    )
+  }
 
   return (
     signed ? <AppRoutes /> : <AuthRoutes />
