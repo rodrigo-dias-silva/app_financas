bd86e2377810d9adbcaf5d526001363dc608ce97
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-11-30 22:19:09 -0300

    adiçao func login e useEffect de loading. adiçao de asyncstorage para armazenar dados de login do user

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,11 +1,53 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import firebase from './../services/firebaseConnection';
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
 
+  useEffect(() => {
+    async function loadStorage() {
+      const storageUser = await AsyncStorage.getItem('Auth_user');
+
+      if (storageUser) {
+        setUser(JSON.parse(storageUser))
+        setLoading(false)
+      }
+
+      setLoading(false)
+    }
+
+    loadStorage();
+  }, [])
+
+  // func login
+  async function signIn(email, password) {
+    await firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
+        await firebase.database().ref('users').child(uid).once('value')
+          .then((snapshot) => {
+            let data = {
+              uid: uid,
+              name: snapshot.val().name,
+              email: value.user.email,
+            }
+
+            setUser(data)
+            storageUser(data)
+          })
+      })
+      .catch((error) => {
+        alert(error.code)
+      })
+  }
+
+
+  // func cadastro
   async function signUp(email, password, nome) {
     await firebase.auth().createUserWithEmailAndPassword(email, password)
       .then(async (value) => {
@@ -20,13 +62,21 @@ function AuthProvider({ children }) {
               name: nome,
               email: value.user.email,
             }
-            setUser(data);
+            setUser(data)
+            storageUser(data)
           })
+      })
+      .catch((error) => {
+        alert(error.code)
       })
   }
 
+  async function storageUser(data) {
+    await AsyncStorage.setItem('Auth_user', JSON.stringify(data))
+  }
+
   return (
-    <AuthContext.Provider value={{ signed: !!user, user, signUp }}>
+    <AuthContext.Provider value={{ signed: !!user, user, signUp, signIn, loading }}>
       {children}
     </AuthContext.Provider>
   );
