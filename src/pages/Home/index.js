4d020ff3b5c23d9322916da463e39f9f0429faaf
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-11-30 23:39:00 -0300

    adiçao button de logout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useContext } from 'react'
 
 import { AuthContext } from '../../contexts/auth'
 
 export default function Home() {
 
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   return (
     <View>
       <Text>Home</Text>
       <Text>{user && user.name}</Text>
       <Text>{user && user.email}</Text>
+
+      <TouchableOpacity onPress={() => signOut()}>
+        <Text>Sair</Text>
+      </TouchableOpacity>
     </View>
   )
 }
