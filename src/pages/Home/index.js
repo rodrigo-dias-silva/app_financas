ee2fe5218421c92650a0b8045e6a1a084aca6fbd
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-11-30 23:11:09 -0300

    adiçao useState para receber info por useContext

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { AuthContext } from '../../contexts/auth'
 
 export default function Home() {
+
+  const { user } = useContext(AuthContext);
+
   return (
     <View>
       <Text>Home</Text>
+      <Text>{user && user.name}</Text>
+      <Text>{user && user.email}</Text>
     </View>
   )
 }
