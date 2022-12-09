c52c27018fb8a82071c764b35ecddfac20f0649a
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-12-09 20:57:37 -0300

    criaçao e estruturaçao do layout da pagina

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,54 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useContext } from 'react'
 
 import { AuthContext } from '../../contexts/auth'
+import Header from './../../components/Header';
 
 export default function Home() {
 
-  const { user, signOut } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   return (
-    <View>
-      <Text>Home</Text>
-      <Text>{user && user.name}</Text>
-      <Text>{user && user.email}</Text>
+    <SafeAreaView style={styles.bg}>
+      <View style={styles.container}>
 
-      <TouchableOpacity onPress={() => signOut()}>
-        <Text>Sair</Text>
-      </TouchableOpacity>
-    </View>
+        <Header />
+        <View style={styles.containerUser}>
+          <Text style={styles.name}>Rodrigo</Text>
+          <Text style={styles.cash}>R$ 123,00</Text>
+        </View>
+        <Text style={styles.title}>Últimas movimentações</Text>
+
+      </View>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  bg: {
+    flex: 1,
+    backgroundColor: '#131313',
+    alignItems: 'center'
+  },
+  container: {
+    width: '90%'
+  },
+  containerUser: {
+    marginBottom: 25
+  },
+  name: {
+    fontSize: 20,
+    color: '#fff',
+    fontStyle: 'italic'
+  },
+  cash: {
+    marginTop: 5,
+    fontSize: 30,
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  title: {
+    color: '#00b94a',
+    marginBottom: 10
+  }
+})
