b17c9d1a45ff7980a478ce9c55d0d677d4de39e2
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-12-09 20:21:57 -0300

    criaçao elemento menu

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+
+export default function Header() {
+  const navigation = useNavigation()
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.btn} onPress={() => navigation.toggleDrawer()}>
+        <Feather name='menu' size={30} color='#fff' />
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 15,
+    marginBottom: 15,
+    width: '90%',
+    height: 50
+  },
+  btn: {
+
+  }
+})
