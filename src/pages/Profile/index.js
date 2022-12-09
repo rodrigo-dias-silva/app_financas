546c8882bc25d90f0e63afb0de4ec303cdca2dd9
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-12-09 18:39:32 -0300

    novas paginas criadas

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,64 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useContext } from 'react'
+import { useNavigation } from '@react-navigation/native';
+
+import { AuthContext } from './../../contexts/auth';
+
+export default function Profile() {
+
+  const navigation = useNavigation()
+
+  const { user, signOut } = useContext(AuthContext)
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>
+        {user && user.name}
+      </Text>
+
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => navigation.navigate('Registrar')}
+      >
+        <Text style={styles.txtBtn}>Registrar gastos</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.btn, { backgroundColor: '#c62c36' }]}
+        onPress={() => signOut()}
+      >
+        <Text style={styles.txtBtn}>Sair</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131313',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  name: {
+    textAlign: 'center',
+    fontSize: 28,
+    marginBottom: 25,
+    marginTop: 25,
+    color: '#fff'
+  },
+  btn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00b94a',
+    width: '90%',
+    height: 45,
+    borderRadius: 10,
+    marginBottom: 10
+  },
+  txtBtn: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold'
+  }
+})
