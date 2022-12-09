c5ba5efd7205a5f31a30e8b7c9af46f09a4a6172
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-12-09 20:58:20 -0300

    criaçao de nova pagina

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, SafeAreaView } from 'react-native';
 
 export default function New() {
   return (
-    <View>
+    <SafeAreaView>
       <Text>New</Text>
-    </View>
+    </SafeAreaView>
   );
 }
