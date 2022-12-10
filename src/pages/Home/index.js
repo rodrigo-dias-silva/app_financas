e58ad90df21c30487a1f59481454ba4989865ee3
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-12-09 22:52:58 -0300

    adiçao do Flatlist + conteudo da lista e alteraçao de css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,26 @@
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useContext } from 'react'
+import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useContext, useState } from 'react'
 
 import { AuthContext } from '../../contexts/auth'
 import Header from './../../components/Header';
+import List from '../../components/List';
 
 export default function Home() {
+
+  const [historico, setHistorico] = useState([
+    { key: '1', type: 'receita', value: 1200 },
+    { key: '2', type: 'despesa', value: 200 },
+    { key: '3', type: 'despesa', value: 1000 },
+    { key: '4', type: 'receita', value: 1200 },
+    { key: '5', type: 'receita', value: 200 },
+    { key: '6', type: 'receita', value: 100 },
+    { key: '7', type: 'despesa', value: 600 },
+    { key: '8', type: 'receita', value: 10 },
+    { key: '9', type: 'despesa', value: 10 },
+    { key: '10', type: 'receita', value: 10 },
+    { key: '11', type: 'despesa', value: 10 },
+    { key: '12', type: 'despesa', value: 110 },
+  ])
 
   const { user } = useContext(AuthContext);
 
@@ -19,6 +35,14 @@ export default function Home() {
         </View>
         <Text style={styles.title}>Últimas movimentações</Text>
 
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={historico}
+          keyExtractor={item => item.key}
+          renderItem={({ item }) => (<List data={item} />)}
+          style={styles.list}
+        />
+
       </View>
     </SafeAreaView>
   )
@@ -31,7 +55,8 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   container: {
-    width: '90%'
+    width: '90%',
+    height: '100%'
   },
   containerUser: {
     marginBottom: 25
@@ -50,5 +75,11 @@ const styles = StyleSheet.create({
   title: {
     color: '#00b94a',
     marginBottom: 10
+  },
+  list: {
+    paddingTop: 15,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15
   }
 })
