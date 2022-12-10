533c35fde3b3fdfbe28d46e2a95fbfebbd3de486
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-12-09 22:51:06 -0300

    criaçao dos cards a serem renderizados na lista

---
+++ src/components/List/index.js
@@ -0,0 +1,58 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Feather } from '@expo/vector-icons';
+
+export default function List({ data }) {
+  return (
+    <View style={[styles.container, styles.shadowContainer]}>
+      <View style={styles.viewType}>
+        <View style={[styles.iconView, { backgroundColor: data.type === 'despesa' ? '#c62c36' : '#049301' }]}>
+          <Feather
+            name={data.type === 'despesa' ? 'arrow-down' : 'arrow-up'}
+            color={'#fff'}
+            size={20}
+          />
+          <Text style={styles.typeTxt}>{data.type === 'despesa' ? 'despesa' : 'receita'}</Text>
+        </View>
+      </View>
+      <Text style={styles.valueTxt}>
+        R$ {data.value}
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 5,
+    padding: 10,
+    backgroundColor: '#f3f4f6',
+    marginHorizontal: 8,
+    borderRadius: 5
+  },
+  shadowContainer: {
+    shadowColor: '#171717',
+    shadowOffset: { width: 4, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  viewType: {
+    flexDirection: 'row'
+  },
+  iconView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    borderRadius: 5
+  },
+  typeTxt: {
+    color: '#fff',
+    fontSize: 16,
+    fontStyle: 'italic'
+  },
+  valueTxt: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+})
