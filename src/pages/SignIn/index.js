3ce75e9bd2e2084feac9cd752e9594b06146ba8f
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-12-19 02:59:48 -0300

    criaçao de botao e funcionalidade de esconder a senha + func de clicar fora e fechar o teclado

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,15 @@
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform, ActivityIndicator } from 'react-native'
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform, ActivityIndicator, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import React, { useState, useContext } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { AuthContext } from './../../contexts/auth';
+import { Feather } from '@expo/vector-icons';
 
 export default function SignIn() {
   const navigation = useNavigation();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [secury, setSecury] = useState(true);
 
   const { signIn, loadingAuth } = useContext(AuthContext);
 
@@ -17,54 +19,63 @@ export default function SignIn() {
 
   return (
     <View style={styles.bg}>
-      <KeyboardAvoidingView
-        style={styles.container}
-        behavior={Platform.OS === 'ios' ? 'padding' : ''}
-        enabled
-      >
-        <Image style={styles.img} source={require('../../images/Logo.png')} />
-        <View style={styles.areaInput}>
-          <TextInput
-            placeholderTextColor="#ffffff66"
-            style={styles.input}
-            placeholder='E-mail'
-            autoCorrect={false}
-            autoCapitalize='none'
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-          />
-        </View>
-        <View style={styles.areaInput}>
-          <TextInput
-            placeholderTextColor="#ffffff66"
-            style={styles.input}
-            placeholder='Senha'
-            autoCorrect={false}
-            autoCapitalize='none'
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-          />
-        </View>
-
-        <TouchableOpacity
-          style={styles.btn}
-          onPress={handleLogin}
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <KeyboardAvoidingView
+          style={styles.container}
+          behavior={Platform.OS === 'ios' ? 'padding' : ''}
+          enabled
         >
-          {
-            loadingAuth ?
-              (<ActivityIndicator size={20} color="#fff" />)
-              :
-              (<Text style={styles.textBtn}>Entrar</Text>)
-          }
-        </TouchableOpacity>
+          <Image style={styles.img} source={require('../../images/Logo.png')} />
+          <View style={styles.areaInput}>
+            <TextInput
+              placeholderTextColor="#ffffff66"
+              style={styles.input}
+              placeholder='E-mail'
+              autoCorrect={false}
+              autoCapitalize='none'
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+          </View>
+          <View style={styles.areaInput}>
+            <TextInput
+              placeholderTextColor="#ffffff66"
+              style={styles.input}
+              placeholder='Senha'
+              autoCorrect={false}
+              autoCapitalize='none'
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry={secury}
+            />
+            <TouchableOpacity
+              style={styles.btnSecury}
+              onPress={() => setSecury(!secury)}
+            >
+              <Text style={styles.txtSecury}>{secury ? (<Feather name='eye' size={20} />) : (<Feather name='eye-off' size={20} />)}</Text>
+            </TouchableOpacity>
+          </View>
 
-        <TouchableOpacity
-          style={styles.link}
-          onPress={() => navigation.navigate("SignUp")}
-        >
-          <Text style={styles.textLink}>Criar uma conta</Text>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={handleLogin}
+          >
+            {
+              loadingAuth ?
+                (<ActivityIndicator size={20} color="#fff" />)
+                :
+                (<Text style={styles.textBtn}>Entrar</Text>)
+            }
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.link}
+            onPress={() => navigation.navigate("SignUp")}
+          >
+            <Text style={styles.textLink}>Criar uma conta</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     </View>
   )
 }
@@ -84,16 +95,30 @@ const styles = StyleSheet.create({
   },
   areaInput: {
     flexDirection: "row",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#ffffff33",
+    borderRadius: 8,
+    width: "90%",
+    height: 45,
+    marginBottom: 15,
   },
   input: {
-    backgroundColor: "#ffffff33",
-    width: "90%",
+    flex: 1,
     fontSize: 16,
     color: "#fff",
-    marginBottom: 15,
     padding: 10,
-    borderRadius: 8,
     height: 45
+  },
+  btnSecury: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 35,
+    marginRight: 8,
+    height: 45,
+  },
+  txtSecury: {
+    color: '#bcbcbc'
   },
   btn: {
     alignItems: "center",
